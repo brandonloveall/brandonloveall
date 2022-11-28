8fe457eeb942c8e06fa4c1c3e14dee8ea86c0359
lazikaz <souflakey@gmail.com>
2022-11-27 21:43:04 -0700

    finished

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let visibility = false
+let dropdown = document.getElementById("menudropdown")
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -5,9 +8,26 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
+
+        visibility = false
+        dropdown.classList.add("invisible")
+        dropdown.classList.remove("visible")
     });
 });
 
+
+
+
 document.getElementById("menu").addEventListener("click", () => {
-    console.log("F")
+    if(visibility == false){
+        visibility = true
+        dropdown.classList.add("visible")
+        dropdown.classList.remove("invisible")
+    }
+    else {
+        visibility = false
+        dropdown.classList.add("invisible")
+        dropdown.classList.remove("visible")
+    }
+    
 })
